54383662d10d2a0790b53acdb688bd5cbff0008f
Kasparas <kasparas.baniulis@gmail.com>
2023-07-03 19:27:58 +0300

    Update test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <main className="App">
       <img src="/favicon.svg" alt="PWA Logo" width="60" height="60" />
-      <h1 className="Home-title">PWA React UPDATED NEW!</h1>
+      <h1 className="Home-title">PWA React UPDATED FINAL!</h1>
       <div className="Home-built">Built at: {date}</div>
       <Outlet />
       <ReloadPrompt />
